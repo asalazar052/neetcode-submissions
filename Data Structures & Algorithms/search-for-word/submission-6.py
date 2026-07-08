d07da97c98a57ba49bc7c3e20e6c4e0d4643186d
asalazar052 <80219999+asalazar052@users.noreply.github.com>
2026-07-07 19:41:17 -0500

    Add: search-for-word - submission-6

---
+++ Data Structures & Algorithms/search-for-word/submission-6.py
@@ -0,0 +1,32 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+
+        ROWS = len(board)
+        COLS = len(board[0])
+        visited = set()
+        res = False
+        
+        def search(curR, curC, curLen):
+            nonlocal res
+
+            if curR < 0 or curR >= ROWS or curC < 0 or curC >= COLS or (curR, curC) in visited or board[curR][curC] != word[curLen]:
+                return
+            
+            if curLen == len(word) - 1:
+                res = True
+                return
+
+            visited.add((curR, curC))
+            search(curR + 1, curC, curLen + 1)
+            search(curR - 1, curC, curLen + 1)
+            search(curR, curC + 1, curLen + 1)
+            search(curR, curC - 1, curLen + 1)
+            visited.remove((curR, curC))
+
+
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                search(r, c, 0)
+        
+        return res
