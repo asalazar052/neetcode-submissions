c80b7d66578ab2478e143fff09e68af21501764d
asalazar052 <80219999+asalazar052@users.noreply.github.com>
2026-07-09 18:10:42 -0500

    Add: combinations-of-a-phone-number - submission-1

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-1.py
@@ -0,0 +1,51 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+
+        res = []
+        curS = ""
+
+        if len(digits) == 0:
+            return []
+
+        mapper = {
+            "2" : "abc",
+            "3" : "def",
+            "4" : "ghi",
+            "5" : "jkl",
+            "6" : "mno",
+            "7" : "pqrs",
+            "8" : "tuv",
+            "9" : "wxyz"
+        }
+
+        combos = set()
+        def helper(i):
+            nonlocal curS
+
+            if i >= len(digits):
+                res.append(curS)
+                return
+            
+            curDigChars = mapper[digits[i]]
+            for j in range(len(curDigChars)):
+                curS += curDigChars[j]
+                helper(i + 1)
+                curS = curS[0:i]
+        
+
+        helper(0)
+        return res
+                
+
+'''
+def
+^
+
+ghi
+ ^
+
+'''
+
+            
+
+
