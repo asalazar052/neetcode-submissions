817a82e1c5c283d6f3d63d2a5bc6da25a1cfe0dc
asalazar052 <80219999+asalazar052@users.noreply.github.com>
2026-07-22 21:02:54 -0500

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.py
@@ -0,0 +1,34 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+        # map each course to prereq list
+        preMap = { i : [] for i in range(numCourses)}
+
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre) # add all prereqs
+        
+        visited = set()
+        def dfs(crs):
+            # Cycle detected
+            if crs in visited:
+                return False
+            
+            # No prereqs required
+            if preMap[crs] == []:
+                return True
+            
+            # Backtracking
+            visited.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre):
+                    return False
+            visited.remove(crs)
+
+            preMap[crs] = [] # So we don't have to do more work
+            return True
+        
+        for i in range(numCourses):
+            if not dfs(i): return False
+        
+        return True
+        
