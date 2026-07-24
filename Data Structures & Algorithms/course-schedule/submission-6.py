da1cd58013f0adb950141bc615e427db3ab92f5d
asalazar052 <80219999+asalazar052@users.noreply.github.com>
2026-07-23 20:07:10 -0500

    Add: course-schedule - submission-6

---
+++ Data Structures & Algorithms/course-schedule/submission-6.py
@@ -0,0 +1,45 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+        # Create adjacency list        
+        coursesToPrereqs = {i : [] for i in range(numCourses)}
+
+
+        '''
+        [0,10],[3,18],[5,5],[6,11],[11,14],[13,1],[15,1],[17,4]
+        0: 10
+        1: 2
+        2: 3
+        3: 18
+        4: 
+        5: 5
+        '''
+        # Load adjacency list
+        for course, prereq in prerequisites:
+            coursesToPrereqs[course].append(prereq)
+        
+        possible = True
+        visited = set()
+
+        def dfs(cur):
+            nonlocal possible
+            # Course has no prerequisites
+            if not coursesToPrereqs[cur]:
+                return
+
+            if cur in visited:
+                possible = False
+                return
+
+            visited.add(cur)
+            for prereq in coursesToPrereqs[cur]:
+                dfs(prereq)
+            visited.remove(cur)
+
+            coursesToPrereqs[cur] = []
+
+            
+        for course, prereq in prerequisites:
+            dfs(course)
+
+        return possible
