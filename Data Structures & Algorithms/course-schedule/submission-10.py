e83084b99dc7ab71f181a376ecf3b44b326cfb5c
asalazar052 <80219999+asalazar052@users.noreply.github.com>
2026-07-24 20:05:27 -0500

    Add: course-schedule - submission-10

---
+++ Data Structures & Algorithms/course-schedule/submission-10.py
@@ -0,0 +1,28 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        courseMap = {i:[] for i in range(numCourses)}
+        for c, p in prerequisites:
+            courseMap[c].append(p)
+
+        valid = True
+        visited = set()
+        def dfs(course):
+            nonlocal valid
+
+            if course in visited:
+                valid = False
+                return
+            if courseMap[course] == []:
+                return
+
+            visited.add(course)
+            for p in courseMap[course]:
+                dfs(p)
+            visited.remove(course)
+            courseMap[course] = []
+
+        for n in range(numCourses):
+            dfs(n)
+        
+        return valid
