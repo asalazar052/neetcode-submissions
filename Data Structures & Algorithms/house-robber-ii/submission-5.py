f194df5c9363629783ac495b1db32ae3bb718d10
asalazar052 <80219999+asalazar052@users.noreply.github.com>
2026-08-08 16:58:42 -0500

    Add: house-robber-ii - submission-5

---
+++ Data Structures & Algorithms/house-robber-ii/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        if len(nums) == 1:
+            return nums[0]
+                
+        def houseRobber1(arr):
+
+            rob1, rob2 = 0, 0 # rob1 is 2 away from cur, rob2 is right next to cur
+            for cur in arr:
+
+                temp = max(rob1 + cur, rob2)
+                rob1 = rob2
+                rob2 = temp
+            
+            return rob2
+
+        return max(houseRobber1(nums[:-1]), houseRobber1(nums[1:]))
