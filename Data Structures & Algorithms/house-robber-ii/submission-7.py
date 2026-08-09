067485614ff92c2ba7ebf63e988f1f521df76569
asalazar052 <80219999+asalazar052@users.noreply.github.com>
2026-08-09 13:55:36 -0500

    Add: house-robber-ii - submission-7

---
+++ Data Structures & Algorithms/house-robber-ii/submission-7.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+        def helper(arr):
+
+            rob1, rob2 = 0, 0 # Rob1 represents 2 houses ago, Rob2 represents the house immediately left
+
+            for house in arr:
+                temp = max(rob1 + house, rob2)
+                rob1 = rob2
+                rob2 = temp
+
+            return rob2
+        
+        return max(nums[0], helper(nums[:-1]), helper(nums[1:]))
