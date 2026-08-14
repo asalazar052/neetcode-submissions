6533fa849ce352e405ec6ad5bc51d016f40ec6c7
asalazar052 <80219999+asalazar052@users.noreply.github.com>
2026-08-13 21:24:16 -0500

    Add: count-number-of-islands - submission-4

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-4.py
@@ -0,0 +1,36 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+
+        visited = set() # keep track of points that we already did work on
+        q = deque()
+        numIslands = 0
+
+        def checkValidity(row, col):
+            if row < 0 or row >= ROWS or col < 0 or col >= COLS or (row, col) in visited or grid[row][col] != "1":
+                return
+            
+            q.append((row, col))
+            grid[row][col] = 67
+
+        def bfs(row, col):
+            
+            q.append((row, col))
+            grid[row][col] = 67
+
+            while q:
+                curRow, curCol = q.popleft()
+                
+                checkValidity(curRow - 1, curCol)
+                checkValidity(curRow + 1, curCol)
+                checkValidity(curRow, curCol - 1)
+                checkValidity(curRow, curCol + 1)
+
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    bfs(r, c)
+                    numIslands += 1
+
+        return numIslands
