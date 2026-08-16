e4b3552abb3fcc83c2210db75de89bf2dd280555
asalazar052 <80219999+asalazar052@users.noreply.github.com>
2026-08-16 09:23:17 -0500

    Add: merge-intervals - submission-1

---
+++ Data Structures & Algorithms/merge-intervals/submission-1.py
@@ -0,0 +1,43 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+
+        def merge_helper(arr1, arr2):
+            start = arr1[0]
+            finish = max(arr1[1], arr2[1])
+            return [start, finish]
+
+
+        intervals.sort()
+        res = [intervals[0]]
+
+        for i in range(1, len(intervals)):
+
+            if res[-1][1] >= intervals[i][0]:
+                res[-1] = merge_helper(res[-1], intervals[i])
+            else:
+                res.append(intervals[i])
+            
+
+        return res
+
+
+
+'''
+[[1, 3], [1, 5], [6, 7]]
+
+res = [1, 2]
+intervals[i] = [2, 3]
+
+
+
+
+
+[1, 5]
+
+if intervals[i][0] >= res[-1][0]:
+    res[-1] = merge_helper(res[-1], intervals[i])
+else:
+    res.append(intervals[i])
+            
+
+'''
